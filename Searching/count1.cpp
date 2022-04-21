ea2c3ba4ba0577493b5d879392e7b8b74ed93fdc
Akriti <akritisingh1811@gmail.com>
2022-04-21 15:02:16 +0530

    Count 1's in binary array

---
+++ Searching/count1.cpp
@@ -0,0 +1,48 @@
+/*
+3. Count 1's in binary array 
+Given a binary sorted non-increasing array of 1s and 0s. You need to print the count of 1s in the binary array.
+
+Example 1:
+
+Input:
+N = 8
+arr[] = {1,1,1,1,1,0,0,0}
+Output: 5
+Explanation: Number of 1's in given 
+binary array : 1 1 1 1 1 0 0 0 is 5.
+Example 2:
+
+Input:
+N = 8
+arr[] = {1,1,0,0,0,0,0,0}
+Output: 2
+Explanation: Number of 1's in given 
+binary array : 1 1 0 0 0 0 0 0 is 2.
+Your Task:
+The task is to complete the function countOne() which takes the array arr[] and its size N as inputs and returns the count of 1s in the input array.
+
+Expected Time Complexity: O(LogN).
+Expected Auxiliary Space: O(1).
+
+Constraint:
+1 <= N <= 5*106
+arr[i] = 0,1
+*/
+
+int countOnes(int arr[], int n)
+    {
+       
+    int s=0,e=n-1,pos=-1;
+    while(s<=e)
+    {
+        int mid=s+(e-s)/2;
+        if(arr[mid]==1)
+        {
+            pos=max(pos,mid);
+            s=mid+1;
+        }
+        else
+        e=mid-1;
+    }
+    return pos+1;
+    }
