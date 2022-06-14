b8c166d049d7f85f3c3ac63295f7b7661a32bf16
Akriti <akritisingh1811@gmail.com>
2022-06-14 12:25:02 +0530

    Jump game updated dp soln

--- Dynamic Programming/jump_game.cpp
+++ Dynamic Programming/jump_game.cpp
@@ -39,3 +39,73 @@ Constraints:
         return last==0;
 
     }
+
+
+
+
+Recursive
+class Solution {
+    public:
+    bool canJump(vector<int>& nums) {
+              return solve(0,nums); 
+    }
+    bool solve(int i,vector<int> nums){
+        
+        if(i==nums.size()-1) return true;
+        int reachable=i+nums[i];
+        
+        for(int k=i+1;k<=reachable;k++){
+                  if(solve(k,nums)){
+                return true;
+            }      
+        }
+        return false;
+    }
+};
+
+
+Recursive+Memoization
+class Solution {
+    vector<int> memo;
+     public:
+     bool canJump(vector<int>& nums) {
+          memo.assign(nums.size(),-1);
+          return solve(0,nums);   
+     }
+     bool solve(int i,vector<int> nums){
+        
+        if(i==nums.size()-1) return true;
+        if(memo[i]!=-1) return memo[i];
+        int reachable=i+nums[i];
+        
+        for(int k=i+1;k<=reachable;k++){
+             if(solve(k,nums)){
+                return memo[i]=true;
+             } 
+        }
+        return memo[i]=false;
+    }
+}
+
+
+
+DP (Updated)
+class Solution {
+    vector<int> memo;
+    public:
+    bool canJump(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> dp(n,0);
+        dp[0]=true;
+        
+        for(int i=1;i<n;i++){
+             for(int j=i-1;j>=0;j--){
+                 if(dp[j] && j+nums[j]>=i){
+                     dp[i]=true;
+                     break;
+                 }       
+             }           
+        }
+        return dp[n-1];
+  }
+};
