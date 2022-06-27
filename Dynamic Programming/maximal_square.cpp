deb81e63671df839631e38385b7acb68ec179f2f
Akriti <akritisingh1811@gmail.com>
2022-06-27 19:36:52 +0530

    Maximal Square

---
+++ Dynamic Programming/maximal_square.cpp
@@ -0,0 +1,52 @@
+/*
+
+Maximal Square
+
+Given an m x n binary matrix filled with 0's and 1's, find the largest square containing only 1's and return its area.
+
+ 
+
+Example 1:
+
+
+Input: matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
+Output: 4
+Example 2:
+
+
+Input: matrix = [["0","1"],["1","0"]]
+Output: 1
+Example 3:
+
+Input: matrix = [["0"]]
+Output: 0
+ 
+
+Constraints:
+
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 300
+matrix[i][j] is '0' or '1'
+
+*/
+
+
+int maximalSquare(vector<vector<char>>& matrix) 
+    {
+        int m=matrix.size(),n=matrix[0].size();
+        vector<vector<int>>dp(m+1,vector<int>(n+1,0)); 
+        int maxlen=0;
+        for(int i=1;i<=m;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(matrix[i-1][j-1]=='1')
+                {
+                    dp[i][j]=min(dp[i-1][j-1],min(dp[i-1][j],dp[i][j-1]))+1;
+                    maxlen=max(maxlen,dp[i][j]);
+                }
+            }
+        }
+        return maxlen*maxlen;
+    }
