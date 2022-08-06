e2d8f19e3c2ba53ba17b168f13851d1fff562c55
Akriti <akritisingh1811@gmail.com>
2022-08-06 15:12:49 +0530

    Shortest Path with Alternating Colors

---
+++ Graph/alternate_col.cpp
@@ -0,0 +1,71 @@
+/*
+
+Shortest Path with Alternating Colors
+
+
+You are given an integer n, the number of nodes in a directed graph where the nodes are labeled from 0 to n - 1. Each edge is red or blue in this graph, and there could be self-edges and parallel edges.
+
+You are given two arrays redEdges and blueEdges where:
+
+redEdges[i] = [ai, bi] indicates that there is a directed red edge from node ai to node bi in the graph, and
+blueEdges[j] = [uj, vj] indicates that there is a directed blue edge from node uj to node vj in the graph.
+Return an array answer of length n, where each answer[x] is the length of the shortest path from node 0 to node x such that the edge colors alternate along the path, or -1 if such a path does not exist.
+
+ 
+
+Example 1:
+
+Input: n = 3, redEdges = [[0,1],[1,2]], blueEdges = []
+Output: [0,1,-1]
+Example 2:
+
+Input: n = 3, redEdges = [[0,1]], blueEdges = [[2,1]]
+Output: [0,1,-1]
+ 
+
+Constraints:
+
+1 <= n <= 100
+0 <= redEdges.length, blueEdges.length <= 400
+redEdges[i].length == blueEdges[j].length == 2
+0 <= ai, bi, uj, vj < n
+
+*/
+
+
+   vector<int> shortestAlternatingPaths(int n, vector<vector<int>>& redEdges, vector<vector<int>>& blueEdges) 
+    {
+        vector<vector<pair<int,int>>> graph(n);
+        
+        for(auto x:redEdges)
+        {
+            graph[x[0]].push_back({x[1],0});
+        }
+        for(auto x:blueEdges)
+        {
+            graph[x[0]].push_back({x[1],1});
+        }
+        
+        queue<vector<int>>q;
+        q.push({0,0,-1});
+        
+        vector<int>dist(n,-1);
+        
+        while(!q.empty())
+        {
+            auto front=q.front();
+            q.pop();
+            
+            dist[front[0]] = (dist[front[0]]!=-1)? dist[front[0]]: front[1];
+            
+            for(auto &adj: graph[front[0]])
+            {
+                if(front[2] != adj.second && adj.first!=-1)
+                {
+                    q.push( vector<int>{adj.first, front[1] +1, adj.second });
+                    adj.first=-1;
+                }
+            }
+        }
+        return dist;
+    }
